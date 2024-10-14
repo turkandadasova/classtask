1e4e55bcc0b5e580b480762a5d130b7db3584cda
Your Name <you@example.com>
2024-10-14 23:43:41 +0400

    first commit

---
+++ classtask1.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car car = new car("brand1", "model1");
+            car.mile = 234;
+            car.ShowFullInfo();
+
+            Biscycle biscycle = new Biscycle("brand2", "model 2");
+            biscycle.mile = 122;
+            biscycle.ShowFullInfo();
+            
+           
+        }
+    }
+
+    public class vehicle
+    {
+        public string brand;
+        public string model;
+        public double mile;
+
+        public vehicle(string brand, string model)
+        {
+            this.brand = brand;
+            this.model = model;
+        }
+    }
+
+    public class car:vehicle
+    {
+        public int DoorsNum;
+
+
+
+        public car(string brand, string model) : base(brand, model)
+        {
+            this.brand = brand;
+            this.model = model;
+        }
+
+
+
+      public void ShowFullInfo()
+       {
+            Console.WriteLine($"brand: {brand}, model: {model} , mile: {mile}");
+        }
+
+
+
+
+    }
+
+
+
+    
+
+
+    public class Biscycle:vehicle
+    {
+        public int WheelNum;
+
+        public Biscycle(string brand,string model):base(brand,model)
+        {
+            
+        }
+
+
+         public void ShowFullInfo()
+         {
+            Console.WriteLine($"brand: {brand}, model: {model} , mile: {mile}" );
+         }
+
+    }
+
+   
+}
---
+++ classtask2.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("name1", 3, 4500, 16);
+            notebook.Count = 100;
+            notebook.Description = "grey";
+            notebook.RAM = 16;
+            notebook.Sale();
+            notebook.ShowFullData();
+            //RAM i sala bilmedim :(
+        }
+    }
+
+  public class Product
+    {
+        public string Name;
+        public string Description;
+        public int Count;
+        public bool IsStock;
+        public double Price;
+
+        public Product(string name,int count,double price)
+        {
+            Name = name;
+            Price = price;
+           
+        }
+    
+    public void ShowFullData()
+        {
+            Console.WriteLine($"name: {Name}, description: {Description}, price: {Price}");
+        }
+    
+    
+    }
+
+
+    public class Notebook:Product 
+    {
+        public byte RAM;
+        public int Storage;
+        
+
+        public Notebook(string Name,int Count,double Price,byte RAM):base(Name,Count,Price )
+        {
+            this.RAM = RAM;
+        }
+
+        public void Sale()
+         
+
+        {
+            if (Count>0)
+            {
+                Console.WriteLine("mehsul satildi :)");
+                Count--;
+                
+            }
+            else
+            {
+                Console.WriteLine("mehsul yoxdu! :(");
+            }
+            
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
